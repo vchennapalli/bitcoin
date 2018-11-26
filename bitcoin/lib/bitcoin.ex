36110def18f98ea253728703725860ef42462cc7
vchennapalli <chennapalli@gmail.com>
2018-11-26 01:12:44 -0500

    add structures for transaction, layout for user generation, global lookup for neighbors

--- bitcoin/lib/bitcoin.ex
+++ bitcoin/lib/bitcoin.ex
@@ -1,18 +1,73 @@
 defmodule Bitcoin do
   @moduledoc """
-  Documentation for Bitcoin.
+  main module from where the peerss are created
   """
+  use Supervisor
+  @doc """
+  main function
+  """
+  def main do
+    num_users = 2
+    {:ok, agent_pid} = Neighbors.start_link(nil)
+    {:ok, super_pid} = start_link(num_users)
+    update_agent(Supervisor.which_children(super_pid), agent_pid)
+
+  end
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Bitcoin.hello()
-      :world
-
+  init for Supervisor
   """
-  def hello do
-    :world
+  def init([]) do
+    nil
   end
+
+  @doc """
+  creates users in the peer-to-peer network
+  """
+  def start_link(num_users) do
+    users = create_users(num_users, [])
+    Supervisor.start_link(users, strategy: :one_for_one)
+  end
+
+  @doc """
+  returns an array of users withi initial state
+  """
+  def create_users(n, users) do
+    if n > 0 do
+      users = [
+        %{
+          id: n,
+          start: {User, :start_link,
+            [%{
+             :wallet => %{
+               :private_key => nil,
+               :public_key => nil,
+               :public_address => nil,
+               :UTXOs => [],
+             },
+              :blockchain => [],
+              :mempool => MapSet.new(),
+            }]
+          }
+        }
+        | users]
+
+      create_users(n-1, users)
+    else
+      users
+    end
+  end
+
+  @doc """
+  adds neighbors to the agent map
+  """
+  def update_agent([child | children], agent_pid) do
+    {id, pid, _, _} = child
+
+    Neighbors.put(agent_pid, id, pid)
+    update_agent(children, agent_pid)
+  end
+
+  def update_agent([], _), do: nil
+
 end
--- bitcoin/lib/user.ex
+++ bitcoin/lib/user.ex
@@ -2,8 +2,62 @@ defmodule User do
   @moduledoc """
   contains the definitions and functions for wallet, blockchain handling, mining etc
   """
+  # use GenServer
+
+  # ASSUMPTIONS
+  # All nodes are full nodes -
+  # - keeps the whole blockchain
+  # - verifies all the rules of Bitcoin
+  # - keeps track of all UTXOs in the network
+
+  # TODO: State
+  # Components of Wallet State - private_key, public_key, public_address, UTXOs
+  # Components of Blockchain - Genesis Block (?)
+  # Mempool
+  #
+  # TODO: Structures
+  # - Block
+  # - Transaction Data Structure
+  #
+  #
+  # TODO: Messeger Functions (for transactions and blocks)
+  # - handle_calls (synchronous)
+  #   - deliver_transaction (from originator to a random connected node in the bitcoin network)
+  #
+  # - handle_info (asynchronous)
+  #   - propagate_transaction (to 3-4 neighbors after validation of transaction. Validation prevents attacks on the network)
+  #   - propagate_block
+  #
+  # TODO: Private Functions
+  # - get_UTXOs(val) -> returns the locked UTXOs whose total value >= val
+  #
+
+  # UNKNOWNS
+  # Coinbase Transactions
+  # Genesis Block
 
 
+  transaction = %{
+    :version => false,
+    :input_counter => 2,
+    :inputs => ['a', 'b'],
+    :output_counter => 1,
+    :outputs => ['c'],
+    :locktime => 0 # default
+  }
 
+  output_structure = %{
+    :value => 1 - 100000000,
+    # :locking_script_size => 1 - 9, # bytes
+    :scriptPubKey => "script"
+  }
+
+  input_structure = %{
+    :transaction_hash => "null",
+    :output_index => "null", # 4 bytes
+    # :unlocking_script_size => 1 - 9, # bytes
+    :scriptSig => "null"
+    # :sequence_number => 0xFFFFFFFF
+  }
 
 end
