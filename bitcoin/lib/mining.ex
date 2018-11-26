63a133ff2cf957dfd7eec156db95498d3d6134a9
akshay <akshay@git>
2018-11-26 16:24:30 -0500

    create proof of work

--- bitcoin/lib/mining.ex
+++ bitcoin/lib/mining.ex
@@ -1,5 +1,6 @@
 defmodule Mining do
 
+#Extrcting the block details and mining the proof of work and updating nonce
     def mine(block) do
         version = block[:version]
         previousblock = block[:previousblock]
@@ -8,25 +9,37 @@ defmodule Mining do
         bits = block[:bits]
         nonce = block[:nonce]
 
-        proofOfwork(version,previousblock,merkelroot,time,bits,nonce)
+        data = proofOfwork(version,previousblock,merkelroot,time,bits,nonce)
+        
+        hash= Enum.at(data,0)
+        nonce = Enum.at(data,1)
+        
+        block = Map.put(block,:blockHash,hash)
+        block = Map.put(block,:nonce,nonce)
+        
+        # IO.puts "#{block[:blockHash]}#{block[:nonce]}"
+        block
+
     end
 
+    #For mining the block
     def proofOfwork(version,previousblock,merkelroot,time,bits,nonce) do
         ha = :crypto.hash(:sha256,"#{version}#{previousblock}#{merkelroot}#{time}#{nonce}")|>Base.encode16
-        IO.inspect ha
-
+        ha = :crypto.hash(:sha256,ha) |> Base.encode16
+        
         if(ha < bits) do
-            ha
+            [ha,nonce]
         else
             nonce = nonce+1
             proofOfwork(version,previousblock,merkelroot,time,bits,nonce)
         end
     end
 
-    # def test do
+    # def test() do
     #     h = :crypto.hash(:sha256, "1")
     #     mr = :crypto.hash(:sha256, "2")
-    #   m = %{:version => 1458,
+    #   m = %{ :blockHash => nil,
+    #     :version => 1458,
     #     :previousblock => h , 
     #     :merkelroot => mr ,
     #     :time => DateTime.utc_now(), 
