388b06936a6c11f2196c1c3c073ca186a0a7e15c
akshay <akshay@git>
2018-11-26 16:23:40 -0500

    Update block structure

--- bitcoin/lib/block.ex
+++ bitcoin/lib/block.ex
@@ -2,19 +2,21 @@ defmodule Block do
 
 #Block structure
     def createBlock() do
-        block = %{ :blockHash: nil,
-        :version: 1, 
-        :previousblock: nil , 
-        :merkelroot: nil ,
-        :time: DateTime.utc_now(), 
-        :bits: nil, 
-        :nonce: 0,
-        :transactions: []
+        block = %{ :blockHash => nil,
+        :version => 1, 
+        :previousblock => "0000000000000000000000000000000000000000000000000000000000000000", 
+        :merkelroot => nil ,
+        :time => DateTime.utc_now(), 
+        :bits => "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 
+        :nonce => 0,
+        :transactions => []
         }
         block
 
     end
-
+    @doc """
+    
+    """
 #CreateMtree function returns the root of the merkel tree formed by the transactions.
     def createMTree(list1) do
         list2 = []
@@ -56,10 +58,68 @@ defmodule Block do
         end
     end
 
-    # def test() do
+    #To verify whether the block created a valid block or not.
+    def verifyBlock(block) do
+        version = block[:version]
+        previousblock = block[:previousblock]
+        merkelroot = block[:merkelroot]
+        time = block[:time]
+        bits = block[:bits]
+        nonce = block[:nonce]
+        blockHash = block[:blockHash]
+
+        result = check(version,previousblock,merkelroot,time,bits,nonce,blockHash)
+        result
+    end
+
+    def check(version,previousblock,merkelroot,time,bits,nonce,blockHash) do
+        ha = :crypto.hash(:sha256,"#{version}#{previousblock}#{merkelroot}#{time}#{nonce}")|>Base.encode16
+        ha = :crypto.hash(:sha256,ha) |> Base.encode16
+        if(blockHash == ha) do
+            true
+        else
+            false
+        end
+    end
+
+# Test block which created a sample block for testing.
+    def testblock() do
+        h = :crypto.hash(:sha256, "previous hash code")
+        mr = :crypto.hash(:sha256, "merkel root header")
+        m = %{ :blockHash => nil,
+        :version => 1,
+        :previousblock => h , 
+        :merkelroot => mr ,
+        :time => DateTime.utc_now(), 
+        #:bits => 1d00ffff
+        :bits => "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 
+        :nonce => 0,
+        :transactions => []
+      }
+      m
+    end
+
+     # def test() do
     #     list = [1,2,3]
     #     createMTree(list)
     # end
 
+    # def test do
+    #     h = :crypto.hash(:sha256, "previous hash code")
+    #     mr = :crypto.hash(:sha256, "merkel root header")
+    #     m = %{ :blockHash => nil,
+    #     :version => 1,
+    #     :previousblock => h , 
+    #     :merkelroot => mr ,
+    #     :time => DateTime.utc_now(), 
+    #     #:bits => 1d00ffff
+    #     :bits => "003fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", 
+    #     :nonce => 0,
+    #     :transactions => []
+    #   }
+    #   block = Mining.mine(m)
+      
+    #   verifyBlock(block)
 
+    # end
 end
